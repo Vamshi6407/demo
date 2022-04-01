5b8223246c3bd41412a12b2419bad3eb2ba1e868
Vamshi6407 <vamshi.krishna@coforge.com>
2022-04-01 19:58:14 +0530

    first commit

---
+++ gitDemoProjectV1/src/gitDemoProjectV1/Test.java
@@ -0,0 +1,20 @@
+package gitDemoProjectV1;
+
+public class Test {
+
+	public static void main(String[] args)
+	{
+	String a = "vamshi";
+	
+	if(a == "vamshi")
+	
+	System.out.println("eat something");
+	
+	else
+	{
+		System.out.println("Do work");
+	}
+	
+	
+	}
+}
